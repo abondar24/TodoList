b26ce36f76c73d408e7dc2fd90ab8850f018b4d2
abondar24 <abondar1992@gmail.com>
2017-09-26 23:39:33 +0200

    Client: add new list form,list table

--- src/main/java/org/abondar/experimental/todolist/app/Application.java
+++ src/main/java/org/abondar/experimental/todolist/app/Application.java
@@ -5,10 +5,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Import;
 
@@ -22,7 +24,10 @@ public class Application extends SpringBootServletInitializer {
     private static Logger logger = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class,args);
+
+        SpringApplicationBuilder builder = new SpringApplicationBuilder(Application.class);
+        builder.web(true).bannerMode(Banner.Mode.OFF).run(args);
+//        SpringApplication.run(Application.class,args);
         logger.info("Application has started");
     }
 }
